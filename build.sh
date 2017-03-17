64c80b265439995c4b4d7683301ba0ae38c07485
Philip Lamb <phil.git@eden.net.nz>
2017-03-17 14:35:51 +1300

    Add build script.

---
+++ build.sh
@@ -0,0 +1,147 @@
+#! /bin/bash
+
+#
+# Build ARToolKit Camera Calibration utility for desktop platforms.
+#
+# Copyright 2016-2017, DAQRI LLC and ARToolKit Contributors.
+# Author(s): Philip Lamb, Thorsten Bux, John Wolf, Dan Bell.
+#
+
+SDK_VERSION='6.0.1'
+SDK_URL_DIR='http://artoolkit-dist.s3.amazonaws.com/artoolkit6/6.0/'
+
+
+# Get our location.
+OURDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+function usage {
+    echo "Usage: $(basename $0) (macos | linux)... "
+    exit 1
+}
+
+if [ $# -eq 0 ]; then
+    usage
+fi
+
+# -e = exit on errors; -x = debug
+set -e -x
+
+# Parse parameters
+while test $# -gt 0
+do
+    case "$1" in
+        osx) BUILD_MACOS=1
+            ;;
+        macos) BUILD_MACOS=1
+            ;;
+        linux) BUILD_LINUX=1
+            ;;
+        --*) echo "bad option $1"
+            usage
+            ;;
+        *) echo "bad argument $1"
+            usage
+            ;;
+    esac
+    shift
+done
+
+
+# Set OS-dependent variables.
+OS=`uname -s`
+ARCH=`uname -m`
+TAR='/usr/bin/tar'
+if [ "$OS" = "Linux" ]
+then
+    CPUS=`/usr/bin/nproc`
+    TAR='/bin/tar'
+elif [ "$OS" = "Darwin" ]
+then
+    CPUS=`/usr/sbin/sysctl -n hw.ncpu`
+elif [ "$OS" = "CYGWIN_NT-6.1" ]
+then
+    CPUS=`/usr/bin/nproc`
+else
+    CPUS=1
+fi
+
+# Function to allow check for required packages.
+function check_package {
+	# Variant for distros that use debian packaging.
+	if (type dpkg-query >/dev/null 2>&1) ; then
+		if ! $(dpkg-query -W -f='${Status}' $1 | grep -q '^install ok installed$') ; then
+			echo "Warning: required package '$1' does not appear to be installed. To install it use 'sudo apt-get install $1'."
+		fi
+	# Variant for distros that use rpm packaging.
+	elif (type rpm >/dev/null 2>&1) ; then
+		if ! $(rpm -qa | grep -q $1) ; then
+			echo "Warning: required package '$1' does not appear to be installed. To install it use 'sudo dnf install $1'."
+		fi
+	fi
+}
+
+function rawurlencode() {
+    local string="${1}"
+    local strlen=${#string}
+    local encoded=""
+    local pos c o
+
+    for (( pos=0 ; pos<strlen ; pos++ )); do
+        c=${string:$pos:1}
+        case "$c" in
+            [-_.~a-zA-Z0-9] ) o="${c}" ;;
+            * )               printf -v o '%%%02x' "'$c"
+        esac
+        encoded+="${o}"
+    done
+    echo -n "${encoded}"
+}
+
+if [ "$OS" = "Darwin" ] ; then
+# ======================================================================
+#  Build platforms hosted by macOS
+# ======================================================================
+
+# macOS
+if [ $BUILD_MACOS ] ; then
+    
+    # Fetch the AR6.framework from latest build into a location where Xcode will find it.
+    SDK_FILENAME="ARToolKit for macOS v${SDK_VERSION}.dmg"
+    curl -f -o "${SDK_FILENAME}" "${SDK_URL_DIR}$(rawurlencode "${SDK_FILENAME}")"
+    hdiutil attach "${SDK_FILENAME}" -noautoopen -quiet -mountpoint "SDK"
+    cp -prf SDK/artoolkit6/SDK/Frameworks/AR6.framework depends/macOS/Frameworks
+    hdiutil detach "SDK" -quiet -force
+    
+    (cd macOS
+    xcodebuild -target "ARToolKit6 Camera Calibration Utility" -configuration Release
+    )
+fi
+# /BUILD_MACOS
+
+fi
+# /Darwin
+
+if [ "$OS" = "Linux" ] ; then
+# ======================================================================
+#  Build platforms hosted by Linux
+# ======================================================================
+
+# Linux
+if [ $BUILD_LINUX ] ; then
+    
+    # Fetch the artoolkit6-dev package and install it.
+    SDK_FILENAME="artoolkit6-dev_${SDK_VERSION}_amd64.deb"
+    curl -f -o "${SDK_FILENAME}" "${SDK_URL_DIR}$(rawurlencode "${SDK_FILENAME}")"
+    sudo dpkg -i "${SDK_FILENAME}"
+
+    (cd linux
+    cmake ..
+    make
+    )
+
+fi
+# /BUILD_LINUX
+
+fi
+# /Linux
+
